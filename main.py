1bc03b0f43e7a20c7881e2afd34d7d43fa3f285f
guigonzalez <guidogermang@gmail.com>
2022-08-17 12:44:29 -0300

    Agrego nuevas funciones y saco cronitor

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import gspread
 from gspread_dataframe import *
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
-import cronitor
 import schedule
 
 
@@ -45,6 +44,13 @@ def createSheet(documento,name,row,col):
     sheet = client.open(documento)
     sheet.add_worksheet(title=name, rows=row, cols=col)
 
+def updateCellByLetter(documento, hoja, cell, data):
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name(credenciales, scope)
+    client = gspread.authorize(creds)
+    sheet = client.open(documento)
+    sheet_instance = sheet.worksheet(hoja)
+    sheet_instance.update(cell, data,value_input_option="USER_ENTERED")
 
 def updateSheetsWithDataframe(documento,hoja,dataframe):
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
@@ -122,7 +128,7 @@ def getComisionEthereum(id, wallet, comision, inicioPeriodoFacturacion, finPerio
                 tempEthWallet = tempEthWallet + monto
 
         totalminado = str(tempEthWallet)#[:7]
-        totalminado = totalminado[:9]
+        totalminado = totalminado[:7]
         clientComision = (float(totalminado) * float(comision) /100)
         clientComision = str(clientComision)[:9]+str(id)
         clientComision = float(clientComision)
@@ -139,7 +145,18 @@ def getUnixTimeStamp(mesInicio,mesFin,diaInicio,diaFin):
     unixFinPeriodoFacturacion = time.mktime(finPeriodoFacturacion.timetuple())
     return unixInicioPeriodoFacturacion, unixFinPeriodoFacturacion
 
-def downloadSheet(spreadsheet_name):
+def downloadSheet(spreadsheet_name,name,wallet,minerType,commissionType,commission,consumption,totalMined,cashPaymentPending,paymentPending):
+    updateCellByLetter("Recibo Housing", "Factura", "B9", name)
+    updateCellByLetter("Recibo Housing", "Factura", "B10", wallet)
+    updateCellByLetter("Recibo Housing", "Factura", "B11", minerType)
+    updateCellByLetter("Recibo Housing", "Factura", "B12", commissionType)
+    updateCellByLetter("Recibo Housing", "Factura", "B13", commission)
+    updateCellByLetter("Recibo Housing", "Factura", "B14", consumption)
+    updateCellByLetter("Recibo Housing", "Factura", "B16", totalMined)
+    updateCellByLetter("Recibo Housing", "Factura", "B18", cashPaymentPending)
+    updateCellByLetter("Recibo Housing", "Factura", "B20", paymentPending)
+    updateCellByLetter("Recibo Housing", "Factura", "E27", cashPaymentPending)
+    updateCellByLetter("Recibo Housing", "Factura", "E24", paymentPending)
     scope = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
@@ -150,8 +167,15 @@ def downloadSheet(spreadsheet_name):
     url = 'https://docs.google.com/spreadsheets/export?format=pdf&id=' + spreadsheet.id
     headers = {'Authorization': 'Bearer ' + creds.create_delegated("").get_access_token().access_token}
     res = requests.get(url, headers=headers)
-    with open(spreadsheet_name + ".pdf", 'wb') as f:
+    with open(name + ".pdf", 'wb') as f:
         f.write(res.content)
+    url = "https://api.telegram.org/bot1956376371:AAFgQ8zc6HLwRReXnzdfN7csz_-iEl8E1oY/sendDocument?chat_id=-634167024"
+    payload = {}
+    files = [
+        ('document', open(f'{name}.pdf','rb'))
+    ]
+    headers= {}
+    response = requests.request("POST", url, headers=headers, data = payload, files = files)
 
 def telegram_message(message):
     headers_telegram = {"Content-Type": "application/x-www-form-urlencoded"}
@@ -167,7 +191,6 @@ def telegram_message(message):
 
 def shouldIRun():
     telegram_message(f"Hoy es: {datetime.now()}")
-
     actualDay = datetime.now().day
     diaInicio, diaFin = getSheetsDataFrame("Cobros - Autom 2.0","INFORMACION")["Ciclo"][0].split(" - ")
     if(int(actualDay) == int(diaInicio)+1):
@@ -179,22 +202,21 @@ def shouldIRun():
 
 
 
-def job():
-    if(shouldIRun()):
+def job(force):
+    if(shouldIRun()or force):
+        updateCellByLetter("Cobros - Autom 2.0", "INFORMACION", "H2", "FALSE")
         telegram_message("Corriendo SCRIPT de Comisiones")
         #VARIABLES
         mesInicio = (datetime.now().month - 1)
         mesFin = datetime.now().month
         diaInicio, diaFin = getSheetsDataFrame("Cobros - Autom 2.0","INFORMACION")["Ciclo"][0].split(" - ")
         gwei_to_eth = 1000000000000000000
-        costPerKwh = float(getSheetsDataFrame("Cobros - Autom 2.0","INFORMACION")["Costo kWh"][0].replace("$",""))
         startDate, endDate = getUnixTimeStamp(mesInicio,mesFin,diaInicio,diaFin)
         clientsDictionary = getSheetsDataFrame("Cobros - Autom 2.0","Clientes Housing").to_dict()
         clientsObjList = []
-
         for i in range (0, len(clientsDictionary["id"])):
             if (clientsDictionary["status"][i]=="TRUE"):
-                cashPaymentPending = float(clientsDictionary["consumo"][i]) * 24 * 30 / 1000 * costPerKwh
+                cashPaymentPending = float(clientsDictionary["consumo"][i]) * 24 * 30 / 1000 * float(clientsDictionary["costo kw"][i])
                 if(clientsDictionary["tipo minero"][i]=="ethereum"):
                     paymentPending, totalMined = getComisionEthereum(clientsDictionary["id"][i], clientsDictionary["wallet"][i], clientsDictionary["comision"][i], startDate, endDate,gwei_to_eth)
                 elif(clientsDictionary["tipo minero"][i]=="bitcoin"):
@@ -205,10 +227,10 @@ def job():
         dfClients = pd.DataFrame(columns=['id', 'nombre', 'comision','total minado', 'moneda', 'mes cripto', 'ars', 'usdt'])
         for x in clientsObjList:
             dfClients.loc[len(dfClients)] = (x.showAll())
-            if x.minerType == "ethereum":
-                telegram_message(f"Cliente: {(x.name)}\nMoneda: Ethereum \nWallet: {(x.wallet)}\nComision: {(x.commission)}\nPeriodo Calculado: {(diaInicio)}/{(mesInicio)} - {(diaFin)}/{(mesFin)}\nTotal Minado por Wallet: {(x.totalMined)}\nA cobrar por PMC: *{(x.paymentPending)}*\n\n*Por favor enviar exactamente: {str(float(x.paymentPending))[:11]} a la siguiente direccion RED BEP20* _(comision de Binance no sumada, por favor agregar antes de transferir)_ \n\nWallet PMC: 0x34fa7b1abfd6e397de3c39934635fedb925eea4d")
-            elif x.minerType == "bitcoin":
-                telegram_message(f"Cliente: {(x.name)}\nMoneda: Bitcoin \nWallet: {(x.wallet)}\nComision: {(x.commission)}\nPeriodo Calculado: {(diaInicio)}/{(mesInicio)} - {(diaFin)}/{(mesFin)}\nTotal Minado por Wallet: {(x.totalMined)}\nA cobrar por PMC ARS: *{(x.cashPaymentPending)}*\nA cobrar por PMC Cripto: *{(x.paymentPending)}*\n\n*Por favor enviar exactamente: {str(float(x.paymentPending))[:11]} a la siguiente direccion RED BEP20* _(comision de Binance no sumada, por favor agregar antes de transferir)_ \n\n*Por favor enviar exactamente: {x.cashPaymentPending} al CBU / ALIAS: 20396565154.LEMON*  \n\nWallet PMC: 0x34fa7b1abfd6e397de3c39934635fedb925eea4d")
+            telegram_message(f"Cliente: {(x.name)}\nMoneda: {x.minerType} \nWallet: {(x.wallet)}\nComision: {(x.commission)}\nPeriodo Calculado: {(diaInicio)}/{(mesInicio)} - {(diaFin)}/{(mesFin)}\nTotal Minado por Wallet: {(x.totalMined)}\nA cobrar por PMC ARS: *{(x.cashPaymentPending)}*\nA cobrar por PMC Cripto: * {str(float(x.paymentPending))[:11]}* a la siguiente direccion RED BEP20* _(comision de Binance no sumada, por favor agregar antes de transferir)_ \n\nWallet PMC: 0x34fa7b1abfd6e397de3c39934635fedb925eea4d")
+            downloadSheet("Recibo Housing",x.name,x.wallet,x.minerType,x.commissionType,x.commission,str(x.consumption),x.totalMined,x.cashPaymentPending,x.
+            paymentPending)
+
 
         print(dfClients)
         try:
@@ -219,14 +241,46 @@ def job():
             formatSheets("Cobros - Autom 2.0",current_month,"A1:H1","titulo")#TITULO
             formatSheets("Cobros - Autom 2.0",current_month,"A1:H"+str(len(clientsDictionary["id"])),"centro")
             formatSheets("Cobros - Autom 2.0",current_month,"F2:F"+str(len(clientsDictionary["id"])-3),"rojo")
-        downloadSheet('Cobros - Autom 2.0')
 
 
+def manualJob(clientId):
+    telegram_message("Corriendo SCRIPT de Comisiones")
+    mesInicio = (datetime.now().month - 1)
+    mesFin = datetime.now().month
+    diaInicio, diaFin = getSheetsDataFrame("Cobros - Autom 2.0","INFORMACION")["Ciclo"][0].split(" - ")
+    gwei_to_eth = 1000000000000000000
+    startDate, endDate = getUnixTimeStamp(mesInicio,mesFin,diaInicio,diaFin)
+    clientDf = getSheetsDataFrame("Cobros - Autom 2.0","Clientes Housing").iloc[clientId-1]
+    if (clientDf["status"]=="TRUE"):
+        cashPaymentPending = float(clientDf["consumo"]) * 24 * 30 / 1000 * float(clientDf["costo kw"])
+        if(clientDf["tipo minero"]=="ethereum"):
+            paymentPending, totalMined = getComisionEthereum(clientDf["id"], clientDf["wallet"], clientDf["comision"], startDate, endDate,gwei_to_eth)
+            telegram_message(f"Cliente: {clientDf['nombre']}\nMoneda: Ethereum \nWallet: {clientDf['wallet']}\nComision: {clientDf['comision']}\nPeriodo Calculado: {diaInicio}/{mesInicio} - {diaFin}/{mesFin}\nTotal Minado por Wallet: {totalMined}\nA cobrar por PMC ARS: *{(cashPaymentPending)}*\nA cobrar por PMC: *{paymentPending}*\n\n*Por favor enviar exactamente: {str(float(paymentPending))[:11]} a la siguiente direccion RED BEP20* _(comision de Binance no sumada, por favor agregar antes de transferir)_\n\n*Por favor enviar exactamente: {cashPaymentPending} al CBU / ALIAS: 20396565154.LEMON* \n\nWallet PMC: 0x34fa7b1abfd6e397de3c39934635fedb925eea4d")
+
+        elif(clientDf["tipo minero"]=="bitcoin"):
+            paymentPending, totalMined = 0,0
+            telegram_message(f"Cliente: {(clientDf['nombre'])}\nMoneda: Bitcoin \nWallet: {(clientDf['wallet'])}\nComision: {(clientDf['comision'])}\nPeriodo Calculado: {(diaInicio)}/{(mesInicio)} - {(diaFin)}/{(mesFin)}\nTotal Minado por Wallet: {(totalMined)}\nA cobrar por PMC ARS: *{(cashPaymentPending)}*\nA cobrar por PMC Cripto: *{(paymentPending)}*\n\n*Por favor enviar exactamente: {str(float(paymentPending))[:11]} a la siguiente direccion RED BEP20* _(comision de Binance no sumada, por favor agregar antes de transferir)_ \n\n*Por favor enviar exactamente: {cashPaymentPending} al CBU / ALIAS: 20396565154.LEMON*  \n\nWallet PMC: 0x34fa7b1abfd6e397de3c39934635fedb925eea4d")
+    else:
+        telegram_message("Cliente desactivado")
+    downloadSheet("Recibo Housing",clientDf['nombre'],clientDf['wallet'],clientDf["tipo minero"],clientDf["tipo comision"],clientDf["comision"],str(clientDf["consumo"]),totalMined,cashPaymentPending,paymentPending)
 runHour = getSheetsDataFrame("Cobros - Autom 2.0","INFORMACION")["Horario Corrida"][0]
 print(runHour)
 schedule.every().day.at(runHour).do(job)
 while True:
-    newRunHour = getSheetsDataFrame("Cobros - Autom 2.0","INFORMACION")["Horario Corrida"][0]
+    informacionDataFrame = getSheetsDataFrame("Cobros - Autom 2.0","INFORMACION")
+    newRunHour = informacionDataFrame["Horario Corrida"][0]
+    manualRun = informacionDataFrame["RUN"][0]
+    if manualRun == "TRUE":
+        idClienteManual = informacionDataFrame["Correr manual"][1]
+        if idClienteManual == 0:
+            job(True)
+        elif idClienteManual > 0:
+            try:
+                manualJob(idClienteManual)
+            except Exception as e:
+                telegram_message("Error corriendo automatismo comisiones de forma MANUAL")
+                telegram_message(e)
+            updateCellByLetter("Cobros - Autom 2.0", "INFORMACION", "H2", "FALSE")
     if runHour != newRunHour:
         telegram_message(f"Automatismo comisiones de Housing - Horario del script cambiado a {newRunHour}")
         exit()
